75d248c3f7de2c45c7efd7e9a9b42702f46ba7b6
Dayane <dayanebmartins@gmail.com>
2022-10-21 10:50:49 -0300

    refatorando req 1

--- src/jobs.py
+++ src/jobs.py
@@ -5,5 +5,5 @@ import csv
 @lru_cache
 def read(path):
     with open("src/jobs.csv", 'r') as file:
-        file_jobs = csv.reader(file)
+        file_jobs = csv.DictReader(file)
     return [file_jobs]
