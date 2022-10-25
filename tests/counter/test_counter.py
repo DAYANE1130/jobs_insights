694ff72be79f23f50513ae10c25c96ff9b2cae88
Dayane <dayanebmartins@gmail.com>
2022-10-25 19:44:49 -0300

    corrige indentaçao requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,8 +1,6 @@
 from src.counter import count_ocurrences
 
+
 def test_counter():
     assert count_ocurrences('src/jobs.csv', 'Python')
-    # print(count_ocurrences('src/jobs.csv', 'Python'))
-    # return count_ocurrences('src/jobs.csv', 'Python')
-
-
+    assert count_ocurrences('src/jobs.csv', 'Javascript')
