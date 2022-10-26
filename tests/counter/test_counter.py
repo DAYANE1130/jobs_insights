47764ee37b0c7022a498a68bbd19a17f3c6b4e42
Dayane <dayanebmartins@gmail.com>
2022-10-26 18:17:28 -0300

    finaliza requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,5 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences('src/jobs.csv', 'Python')
-    assert count_ocurrences('src/jobs.csv', 'Javascript')
+    assert count_ocurrences("src/jobs.csv", "Python") == 1639
+    assert count_ocurrences("src/jobs.csv", "Javascript") == 122
